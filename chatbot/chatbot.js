9b21c40cf0f07f23656256b5a3c998ad93341e3d
leandrolazzaretti <leandrolazzaretti@outlook.com>
2022-04-10 20:31:22 +0100

    feat: add chatbot module

---
+++ chatbot/chatbot.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const dialogFlow = require('dialogFlow');
+const config = require('../config/keys');
+
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const credentials = {
+  client_email: config.googleClientEmail,
+  private_key: config.googlePrivateKey
+}
+
+const sessionClient = new dialogFlow.SessionsClient({projectId, credentials});
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+module.exports = {
+  textQuery: async function (text, parameters = {}) {
+    let self = module.exports;
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: text,
+          languageCode: config.dialogFlowSessionLanguageCode,
+        },
+      },
+      queryParams: {
+        payload: {
+          data: parameters
+        }
+      }
+    };
+    let responses = await sessionClient.detectIntent(request);
+    responses = await self.handleAction(responses);
+    return responses;
+  },
+  handleAction: function (responses) {
+    return responses;
+  }
+}
