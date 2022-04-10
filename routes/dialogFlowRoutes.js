b3da44e6c490ec412dfeb131ea57488fa0bb17f4
leandrolazzaretti <leandrolazzaretti@outlook.com>
2022-04-10 13:30:17 +0100

    feat: add df routes

---
+++ routes/dialogFlowRoutes.js
@@ -0,0 +1,31 @@
+const dialogFlow = require('dialogFlow');
+const config = require('../config/keys');
+const sessionClient = new dialogFlow.SessionsClient();
+
+const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+
+module.exports = app => {
+  app.get('/', (req, res) => {
+    res.send({'hello': 'Jhonyy'});
+  });
+
+  app.post('/api/df_text_query', async (req, res) => {
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: req.body.text,
+          languageCode: config.dialogFlowSessionLanguageCode,
+        },
+      },
+    };
+    let responses = await sessionClient
+      .detectIntent(request);
+      
+    res.send(responses[0].queryResult);
+  });
+
+  app.post('/api/df_event_query', (req, res) => {
+    res.send({'do': 'event query'});
+  });
+}
