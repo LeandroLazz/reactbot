9c859b724e44380ed5a34a3852219834a86acd76
leandrolazzaretti <leandrolazzaretti@outlook.com>
2022-04-12 22:38:16 +0100

    feat: change index

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
 require('./routes/dialogFlowRoutes')(app);
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
