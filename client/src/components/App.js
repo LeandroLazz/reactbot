4b60c90163b56d8856ef81d4d3cbb1c71c7db7c5
leandrolazzaretti <leandrolazzaretti@outlook.com>
2022-04-13 20:29:45 +0100

    feat: add client

---
+++ client/src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Landing from './pages/Landing';
+import About from './pages/About';
+import Shop from './shop/Shop';
+import Chatbot from './chatbot/Chatbot';
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div className="container">
+        <Header/>
+        <Routes>
+          <Route exact path="/" element={ <Landing/> }/>
+          <Route exact path="/about" element={ <About/> }/>
+          <Route exact path="/shop" element={ <Shop/> }/>
+        </Routes>
+        <Chatbot/>
+      </div>
+    </BrowserRouter>
+  </div>
+)
+
+export default App;
