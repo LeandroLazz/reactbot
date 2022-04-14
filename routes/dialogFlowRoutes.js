3822f2d5fc2405a3c45ccb97c01d622d4bd286ba
leandrolazzaretti <leandrolazzaretti@outlook.com>
2022-04-14 22:22:09 +0100

    feat: remove hello route

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,9 +1,9 @@
 const chatbot = require('../chatbot/chatbot');
 
 module.exports = app => {
-  app.get('/', (req, res) => {
-    res.send({'hello': 'Jhonyy'});
-  });
+  // app.get('/', (req, res) => {
+  //   res.send({'hello': 'My friend!!!'});
+  // });
 
   app.post('/api/df_text_query', async (req, res) => {
     let responses = await chatbot.textQuery(req.body.text, req.body.userID, req.body.parameters);
