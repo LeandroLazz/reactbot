4a64a6152329c738b4478b2c30fc45ae77acb3bb
leandrolazzaretti <leandrolazzaretti@outlook.com>
2022-04-14 10:46:19 +0100

    feat: add cards component

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -38,6 +38,7 @@ class Chatbot extends Component {
     const res = await axios.post('/api/df_text_query', {text: queryText, userID: cookies.get('userID')});
 
     for (let msg of res.data.fulfillmentMessages) {
+      console.log(JSON.stringify(msg));
       says = {
         speaks: 'bot',
         msg: msg
@@ -69,7 +70,11 @@ class Chatbot extends Component {
   renderMessages(stateMessages) {
     if (stateMessages) {
       return stateMessages.map((message, i) => {
-        return <Message key={i} speaks={message.speaks} text={message.msg.text.text} />;
+        if (message.msg && message.msg.text && message.msg.text.text) {
+          return <Message key={i} speaks={message.speaks} text={message.msg.text.text} />;
+        } else {
+          <h2>Cards</h2>
+        }
       });
     } else {
       return null;
