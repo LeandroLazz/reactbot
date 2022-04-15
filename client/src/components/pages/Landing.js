cb36d0fa38debb1244c5624463b53ec258deb57e
leandrolazzaretti <leandrolazzaretti@outlook.com>
2022-04-15 12:14:25 +0100

    feat: add html instructions

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Landing = () => (
-  <div style={{ textAlign: 'center' }}>
-    <h1>Selling you great stuff!</h1>
-    with the help of the chatbot
+  <div style={{ textAlign: 'left' }}>
+    <h4>with the help of the chatbot</h4>
+    <h5>try some of the messages below:</h5>
+    <p>
+      - hi<br/>
+      - Can you recommend a course?<br/>
+      - I want to learn about javaScript<br/>
+      - I would like to learn about chatbot<br/>
+    </p>
   </div>   
 )
 
